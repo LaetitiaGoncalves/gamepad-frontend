25c822d3a7567ba2c096b4ecb01a66d82543e190
Laetitia Goncalves <laetitiagoncalves@macbook-pro-de-laetitia.home>
2022-08-16 12:33:11 +0200

    adding signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUser,
+  faBookmark,
+  faCommentDots,
+} from "@fortawesome/free-regular-svg-icons";
+
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Header from "./components/Header";
 
 function App() {
+  library.add(faUser, faBookmark, faCommentDots);
   return (
     <div>
       <Router>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,48 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Signup = () => {
   return (
     <div>
-      <div>
+      <div className="howItWorks">
         <h2>How it Works ?</h2>
+        <div>
+          <FontAwesomeIcon icon="fa-regular fa-user" />
+          <p>
+            Log in to your free account to be able to get all features of
+            Gamepad
+          </p>
+        </div>
+        <div>
+          <FontAwesomeIcon icon="fa-regular fa-bookmark" />
+          <p>Add a game to your collection</p>
+        </div>
+        <div>
+          <FontAwesomeIcon icon="fa-regular fa-comment-dots" />
+          <p>Leave a review for a game</p>
+        </div>
       </div>
-      <div></div>
+      <div className="signup-card">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" name="" id="" placeholder="Password" />
+          <input type="password" name="" id="" placeholder="Confirm Password" />
+          <div>
+            <label>
+              <input type="file" style={{ display: "none" }} />
+              Add a Photo
+            </label>
+            <p>No file selected</p>
+          </div>
+          <div>
+            <label>
+              <input type="submit" style={{ display: "none" }} />
+              Envoyer
+            </label>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
